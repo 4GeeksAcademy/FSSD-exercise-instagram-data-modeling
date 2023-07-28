4059e10cd10676f690d398f2aa388dffd97d3cce
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-07-28 17:41:33 +0000

    Instagram database model.py

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -8,25 +9,46 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class Person(Base):
-    __tablename__ = 'person'
+    __tablename__ = 'Person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
+    created_at = Column(DateTime, server_default='CURRENT_TIMESTAMP')
 
-class Address(Base):
-    __tablename__ = 'address'
+    posts = relationship("Post", backref="user")
+    comments = relationship("Comment", backref="user")
+    likes = relationship("Like", backref="user")
+
+class Post(Base):
+    __tablename__ = 'Post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    person_id = Column(Integer, ForeignKey('Person.id'), nullable=False)
+    content = Column(Text, nullable=False)
+    media_url = Column(String(255), nullable=False)
+    created_at = Column(DateTime, server_default='CURRENT_TIMESTAMP')
 
-    def to_dict(self):
-        return {}
+    comments = relationship("Comment", backref="post")
+    likes = relationship("Like", backref="post")
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('Person.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
+    comment_text = Column(Text, nullable=False)
+    created_at = Column(DateTime, server_default='CURRENT_TIMESTAMP')
+
+class Like(Base):
+    __tablename__ = 'Like'
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('Person.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
+    created_at = Column(DateTime, server_default='CURRENT_TIMESTAMP')
 
 ## Draw from SQLAlchemy base
 try:
